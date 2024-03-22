119a91a3ac6351ee8d1bd33caedd05c3dbdb4e4c
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:53:04 +0100

    Added solution for TwoSum

---
+++ Problems/Easy/TwoSum.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Problems.Easy
+{
+    internal class TwoSum
+    {
+        internal static int[] Solution(int[] nums, int target)
+        {
+            int[] output = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target && j != i)
+                    {
+                        output[0] = i;
+                        output[1] = j;
+
+                        break;
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
