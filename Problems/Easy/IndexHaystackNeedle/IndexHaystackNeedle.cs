c873f3206dfc7e57f6d19818cf4b58728b9d6b6b
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:53:05 +0100

    Added solution Problem: 28. Find the Index of the First Occurrence in a String

---
+++ Problems/Easy/IndexHaystackNeedle/IndexHaystackNeedle.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Problems.Easy.IndexHaystackNeedle
+{
+    internal class IndexHaystackNeedle
+    {
+        internal static int Solution(string haystack, string needle)
+        {
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack.Substring(i, needle.Length).Equals(needle))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
