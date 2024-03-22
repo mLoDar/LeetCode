c08d8931c4b2fc6b1991192c4c662d047c89c95e
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:53:05 +0100

    Added solution Problem: 66. Plus One

---
+++ Problems/Easy/PlusOne/PlusOne.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace LeetCode.Problems.Easy.PlusOne
+{
+    internal class PlusOne
+    {
+        internal static int[] Solution(int[] digits)
+        {
+            var strNum = BigInteger.Parse(string.Join("", digits)) + 1;
+
+            int[] output = new int[strNum.ToString().Length];
+
+            for (int i = 0; i < strNum.ToString().Length; i++)
+            {
+                output[i] = Convert.ToInt32(strNum.ToString().Substring(i, 1));
+            }
+
+            return output;
+        }
+    }
+}
