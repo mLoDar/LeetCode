492374ea12b683bff80fde21254d243b30907902
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:53:05 +0100

    Added solution Problem: 69. Sqrt(x)

---
+++ Problems/Easy/MySqrt/MySqrt.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode.Problems.Easy.MySqrt
+{
+    internal class MySqrt
+    {
+        internal static int Solution(int x)
+        {
+            double squareRoot = Math.Sqrt(x);
+            string searchedNum = Convert.ToString(squareRoot).Split('.')[0];
+
+            return Convert.ToInt32(searchedNum);
+        }
+    }
+}
