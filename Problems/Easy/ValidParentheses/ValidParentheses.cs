f7e91ca352bf8df46b0fdff6c264a915fd11698d
mLoDar <mlodarcoding@gmail.com>
2024-03-22 21:53:05 +0100

    Added solution for problem "ValidParantheses"

---
+++ Problems/Easy/ValidParentheses/ValidParentheses.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Problems.Easy.ValidParentheses
+{
+    internal class ValidParentheses
+    {
+        internal static bool Solution(string s)
+        {
+            string openedParentheses = string.Empty;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                bool opened = false;
+
+                switch (s[i])
+                {
+                    case '(':
+                        openedParentheses = ')' + openedParentheses;
+                        opened = true;
+                        break;
+
+                    case '{':
+                        openedParentheses = '}' + openedParentheses;
+                        opened = true;
+                        break;
+
+                    case '[':
+                        openedParentheses = ']' + openedParentheses;
+                        opened = true;
+                        break;
+                }
+
+                if (openedParentheses.Length < 0 || opened == true)
+                {
+                    continue;
+                }
+
+                if (openedParentheses.Length > 0 && s[i].Equals(openedParentheses.First()))
+                {
+                    openedParentheses = openedParentheses.Remove(0, 1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (openedParentheses.Length > 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
